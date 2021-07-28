0791bf74f3d1f781a2415e7b874179848d8f5462
DylanBruner <61059252+DylanBruner@users.noreply.github.com>
2021-07-28 11:35:07 -0400

    Update screenlib.py

--- sd-card/modules/screenlib.py
+++ sd-card/modules/screenlib.py
@@ -26,9 +26,16 @@ class screenlib_screen_button(object):
 
     def draw(self):
         if not self.hide:
+            if self.w == self.h:
+                l = 2
+            else:
+                l = 4
+
             brain.screen.set_fill_color(self.c)
             brain.screen.set_pen_color(Color.PURPLE)
             brain.screen.draw_rectangle(self.x,self.y,self.w,self.h,self.c)
+            brain.screen.draw_circle(self.x+self.w,self.y+self.h/2,self.w/l)
+            brain.screen.draw_circle(self.x,self.y+self.h/2,self.w/l)
 
             brain.screen.set_fill_color(self.c)
             brain.screen.set_cursor(self.tc,self.tr)
@@ -87,5 +94,46 @@ class screenlib_screen_menu(object):
         brain.screen.set_fill_color(Color.BLUE)
         brain.screen.print(text)
         wait(2.5,SECONDS)
+
+class screenlib_controller_menu(object):
+    def __init__(self):
+        self.items = []
+        self.selectedItem = 0
+
+    def create_item(self, text):
+        self.items.append(text)
+    
+    def create_items(self, items):
+        for x in items:
+            self.items.append(x)
+    
+    def draw(self):
+        controller_1.screen.clear_screen()
+        controller_1.screen.set_cursor(0,0)
+        for item in self.items:
+            if self.items.index(item) == self.selectedItem:
+                controller_1.screen.print("> "+item)
+            
+            else:
+                controller_1.screen.print("  "+item)
+            controller_1.screen.set_cursor(controller_1.screen.row()+1, 0)
+    
+    def run(self):
+        self.draw()
+
+        while True:
+            if controller_1.buttonDown.pressing():
+                if len(self.items) - 1 != self.selectedItem:
+                    self.selectedItem += 1
+                    self.draw()
+                
+            elif controller_1.buttonUp.pressing():
+                if self.selectedItem != 0:
+                    self.selectedItem -= 1
+                    self.draw()
+                wait(.1,SECONDS)
+            
+            elif controller_1.buttonRight.pressing():
+                return self.items[self.selectedItem]
 
 print("[module/screenlib]: Loaded")
